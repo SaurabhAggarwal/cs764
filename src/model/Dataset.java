3ae9d95987c570095f2793b4e9b99347c7565f35
Saurabh Aggarwal <SaurabhAgg@gmail.com>
2013-03-15 00:04:00 -0500

    Modified Dataset

--- src/model/Dataset.java
+++ src/model/Dataset.java
@@ -16,29 +16,37 @@ package model;
  *
  */
 public enum Dataset {
-	SIMPLE(3, 2),
-	T5_I2_D100K(5, 2),
-	T10_I2_D100K(10, 2),
-	T10_I4_D100K(10, 4),
-	T20_I2_D100K(20, 2),
-	T20_I4_D100K(20, 4),
-	T20_I6_D100K(20, 6);
+
+	SIMPLE(3, 2, 4),
+	T5_I2_D100K(5, 2, 97048),
+	T10_I2_D100K(10, 2, 99916),
+	T10_I4_D100K(10, 4, 98395),
+	T20_I2_D100K(20, 2, 100000),
+	T20_I4_D100K(20, 4, 99996),
+	T20_I6_D100K(20, 6, 99942);
+
+	private int avgTxnSize, avgMaxLargeItemsetSize;
+	private int numTxns;
 	
-	int I, T;
-	
-	private Dataset(int T, int I)
+	private Dataset(int avgTxnSize, int avgMaxLargeItemsetSize, int numTxns)
 	{
-		this.T = T;
-		this.I = I;
+		this.avgTxnSize = avgTxnSize;
+		this.avgMaxLargeItemsetSize = avgMaxLargeItemsetSize;
+		this.numTxns = numTxns;
 	}
 	
 	public int getAvgTxnSize()
 	{
-		return this.T;
+		return this.avgTxnSize;
 	}
 	
 	public int getAvgMaxLargeItemsetSize()
 	{
-		return this.I;
+		return this.avgMaxLargeItemsetSize;
+	}
+	
+	// Number of distinct transactions in this dataset.
+	public int getNumTxns() {
+		return numTxns;
 	}
 }
